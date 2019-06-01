81effdf5de3d82c89c56052212de10e6c36bdcf0
llUJO <jirapan_yankhan@hotmail.com>
2019-06-01 23:29:54 +0700

    [ EDIT ] auto request

--- frontend/src/app/score/score.component.ts
+++ frontend/src/app/score/score.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-score',
@@ -7,11 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreComponent implements OnInit {
 
-
-  constructor() {
+  private updateSubscription: Subscription;
+  private listWIP: any = [];
+  constructor(
+    private http: HttpClient
+  ) {
   }
+
 
   ngOnInit() {
+    this.updateSubscription = interval(3000).subscribe(
+      (val) => {
+        this.updateStats();
+      }
+    );
+
+
   }
+
+  ngOnDestroy() {
+    this.updateSubscription.unsubscribe();
+  }
+
+  private updateStats() {
+    this.http.get("http://localhost:3001/getListWIP").subscribe(val => {
+      this.listWIP = val;
+    })
+
+    // console.log(this.listWIP);
+
+  }
+
+  private swapData(){
+
+    var myData = ["1", "2", "3", "5", "4", "6", "7"];
+
+    // for (let i = 0; i < myData.length - 1; i++)
+    //   for (let j = 0; j < myData.length - i - 1; j++)
+    //     if (myData[j] < myData[j + 1]) {
+    //       // swap arr[j+1] and arr[i] 
+    //       let temp = myData[j];
+    //       myData[j] = myData[j + 1];
+    //       myData[j + 1] = temp;
+    //     }
+   
+  }
+
 
 }
