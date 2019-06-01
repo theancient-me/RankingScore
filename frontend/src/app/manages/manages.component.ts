12bdda974674434e6a1b78ebedcfd9dad7604638
llUJO <jirapan_yankhan@hotmail.com>
2019-06-01 23:28:37 +0700

    [ EDIT ] Auto request list

--- frontend/src/app/manages/manages.component.ts
+++ frontend/src/app/manages/manages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, interval, Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-manages',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagesComponent implements OnInit {
 
-  constructor() { }
+  private listWIP:any;
+  private updateSubscription: Subscription;
+
+  constructor(
+    private http : HttpClient
+  ) { }
 
   ngOnInit() {
+    this.updateSubscription = interval(5000).subscribe(
+      (val) => { this.updateStats()
+    }
+);
   }
 
+ngOnDestroy() {
+  this.updateSubscription.unsubscribe();
+}
+  
+
+private updateStats() {
+  this.http.get("http://localhost:3001/getListWIP").subscribe(val =>{
+    this.listWIP = val;
+  })
+}
 }
