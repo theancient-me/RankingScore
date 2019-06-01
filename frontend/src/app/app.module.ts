b0f4045e13e6e186e91a5bed6eb4ec0968a992b2
llUJO <jirapan_yankhan@hotmail.com>
2019-06-01 13:31:13 +0700

    [ ADD ] Login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScoreComponent } from './score/score.component';
+import { LoginComponent } from './login/login.component';
+import { ManagesComponent } from './manages/manages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScoreComponent
+    ScoreComponent,
+    LoginComponent,
+    ManagesComponent
   ],
   imports: [
     BrowserModule,
